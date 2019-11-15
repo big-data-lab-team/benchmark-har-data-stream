2783d40b9483b61d87ef4a82e24652104e872a70
Martin <martin.khannouz@gmail.com>
2019-11-15 10:01:21 -0500

    Update Makefile.

--- Makefile
+++ Makefile
@@ -1,8 +1,36 @@
-all:
-	python makefile.py compile
-compile:
-	python makefile.py compile
+OrpailleCC_DIR=/home/magoa/phd/OrpailleCC
+OrpailleCC_INC=$(OrpailleCC_DIR)/src
+LABEL_COUNT=2
+FEATURES_COUNT=3
+
+ifeq ($(config), debug)
+DEBUG_FLAGS=-DDEBUG -g -O0 $(FLAG_GCOV)
+else #release config by default
+DEBUG_FLAGS=-Os -O3
+endif
+
+COMMON_FLAGS=-std=c++11 -I$(OrpailleCC_INC) -DLABEL_COUNT=$(LABEL_COUNT) -DFEATURES_COUNT=$(FEATURES_COUNT) $(DEBUG_FLAGS)
+
+compile: AppPowerMeter mondrian_t10 empty_classifier
+
+mondrian_t%: mond.cpp
+	#$* contains everything within "%" of the target
+	g++ main.cpp  $(COMMON_FLAGS)\
+		-DCLASSIFIER_INITIALIZATION_FILE="\"mond.cpp\"" -DTREE_COUNT=$* -o $@
+
+empty_classifier:
+	g++ main.cpp $(COMMON_FLAGS) \
+		-DCLASSIFIER_INITIALIZATION_FILE="\"empty.cpp\"" -DTREE_COUNT=10 -o $@
+
+AppPowerMeter:
+	$(MAKE) -C rapl-tools
+	cp rapl-tools/AppPowerMeter rapl-tools/PowerMonitor .
+
 latex:
 	python makefile.py latex
 dataset:
 	python makefile.py dataset
+clean:
+	$(MAKE) -C rapl-tools clean
+	rm -rf mondrian_t* empty_classifier
+	rm -f AppPowerMeter PowerMonitor
--- makefile.py
+++ makefile.py
@@ -4,41 +4,12 @@ import math
 import os
 from statistics import mean
 
-def mondrian_forest_command(tree_count):
-    return ["-DCLASSIFIER_INITIALIZATION_FILE=\"mond.cpp\"", \
-            "-DTREE_COUNT=" + str(tree_count), \
-            "-o", "mondrian_t" + str(tree_count)]
-
-def empty_classifier_command():
-    return ["-DCLASSIFIER_INITIALIZATION_FILE=\"empty.cpp\"", \
-            "-o", "empty_classifier"]
-
 def rangy(mini, maxi, step):
     return [round(mini + x * step, 2) for x in range(0, math.floor((maxi-mini)/step))]
 
 #Make all the binary
 def compile():
-    compilation_list = []
-
-    OrpailleCC_DIR = "/home/magoa/phd/OrpailleCC"
-    OrpailleCC_INC = OrpailleCC_DIR + "/src"
-
-    base_compilation_command = ["g++", "-std=c++11", \
-                                "-I" + OrpailleCC_INC,  "main.cpp", \
-                                "-DLABEL_COUNT=2", "-DFEATURES_COUNT=3"]
-
-    compilation_list.append(base_compilation_command + mondrian_forest_command(20))
-    compilation_list.append(base_compilation_command + empty_classifier_command())
-    # for  lifetime in rangy(0.5, 3.0, 0.1):
-        # for  base in rangy(0.0, 1.0, 0.1):
-            # for d in rangy(0.1, 2.0, 0.1):
-                # compilation_list.append(base_compilation_command + mondrian_forest_command(lifetime, base, d, 20))
-
-    for command in compilation_list:
-        print(" ".join(command))
-        command_result = subprocess.call(command)
-
-    subprocess.call(["make"], cwd="rapl-tools/")
+    subprocess.call(["make"])
 
 def latex():
     subprocess.call(["make"], cwd="paper/")
@@ -64,7 +35,6 @@ def process_file(in_f, out_f, window_size):
             features = extract_features(window)
             window = []
             out_f.write("\t".join(features) + "\n")
-
 
 #Build the dataset
 def dataset():
