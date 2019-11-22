2ce8d9bcbd41dafca4bb3bfffc93ad8a03ab0648
Martin <martin.khannouz@gmail.com>
2019-11-22 11:02:37 -0500

    Add memory leak function in EmptyClassifier. Format output of EmptyClassifier. Add a template parameter in EmptyClassifier to define the default prediction.

--- empty.cpp
+++ empty.cpp
@@ -1,4 +1,4 @@
-template<class feature_type>
+template<class feature_type, int default_prediction=1>
 class EmptyClassifier{
 	public:
 		EmptyClassifier(){
@@ -6,11 +6,18 @@ class EmptyClassifier{
 	inline bool train(feature_type const* features, int const label){
 		return true;
 	}
+	inline void memory_leak(void){
+		int const hehe = 1024;
+		int* c = new int[hehe];
+		for(int i = 0; i < hehe; ++i)
+			c[i] = 1;
+	}
 	inline int predict(feature_type const* features){
-		return 0;
+		memory_leak();
+		return default_prediction; //1 because 0 is when there is no activity therefore 0 is more dominant
 	}
 };
 EmptyClassifier<double>* get_classifier(int argc, char** argv){
-	cout << "EmptyClassifier is instanciated." << endl;
+	cout << "Model: Empty" << endl;
 	return new EmptyClassifier<double>();
 }
