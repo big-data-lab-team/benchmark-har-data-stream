c6d35d318731442054f18f218db560e410cd4e5b
Martin <martin.khannouz@gmail.com>
2019-11-22 10:41:25 -0500

    Fix bug in template parameter.

--- mond.cpp
+++ mond.cpp
@@ -1,7 +1,7 @@
 #include <mondrian.hpp>
 
 
-MondrianForest<double, functions, TREE_COUNT, LABEL_COUNT, FEATURES_COUNT, 60000>* get_classifier(int argc, char** argv){
+MondrianForest<double, functions, TREE_COUNT, FEATURES_COUNT, LABEL_COUNT, 600000>* get_classifier(int argc, char** argv){
 	if(argc < 3){
 		cout << "usage: <lifetime> <base_measure> <discount_factor>" << endl;
 		return nullptr;
@@ -10,7 +10,8 @@ MondrianForest<double, functions, TREE_COUNT, LABEL_COUNT, FEATURES_COUNT, 60000
 	double const base_measure = stod(argv[1]);	
 	double const discount_factor = stod(argv[2]);	
 
+	cout << "Model: Mondrian "; //All parameters forms the name of the model
 	cout << "Lifetime: " << lifetime << " Base measure: " << base_measure << " Discount factor: " << discount_factor << " Tree count: " << TREE_COUNT << endl;
-	return new MondrianForest<double, functions, TREE_COUNT, LABEL_COUNT, FEATURES_COUNT, 60000>(lifetime, base_measure, discount_factor);
+	return new MondrianForest<double, functions, TREE_COUNT, FEATURES_COUNT, LABEL_COUNT, 600000>(lifetime, base_measure, discount_factor);
 }
 
