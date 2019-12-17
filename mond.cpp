39f8129d78c22d5fc092e73730bc9b381fe3eb2e
Martin <martin.khannouz@gmail.com>
2019-12-17 16:44:43 -0500

    Add SIZE to control the size of Mondrian.

--- mond.cpp
+++ mond.cpp
@@ -1,7 +1,9 @@
 #include <mondrian.hpp>
 
 
-MondrianForest<double, functions, TREE_COUNT, FEATURES_COUNT, LABEL_COUNT, 600000>* get_classifier(int argc, char** argv){
+#define SIZE 600000
+
+MondrianForest<double, functions, TREE_COUNT, FEATURES_COUNT, LABEL_COUNT, SIZE>* get_classifier(int seed, int argc, char** argv){
 	if(argc < 3){
 		cout << "usage: <lifetime> <base_measure> <discount_factor>" << endl;
 		return nullptr;
@@ -10,12 +12,7 @@ MondrianForest<double, functions, TREE_COUNT, FEATURES_COUNT, LABEL_COUNT, 60000
 	double const base_measure = stod(argv[1]);	
 	double const discount_factor = stod(argv[2]);	
 
-	cout << "Model: Mondrian "; //All parameters forms the name of the model
-	cout << "Lifetime: " << lifetime << " Base measure: " << base_measure << " Discount factor: " << discount_factor << " Tree count: " << TREE_COUNT << endl;
-	int seed = time(nullptr);
-	cout << "Seed: " << seed << endl;
 	srand(seed);
-		
-	return new MondrianForest<double, functions, TREE_COUNT, FEATURES_COUNT, LABEL_COUNT, 600000>(lifetime, base_measure, discount_factor);
+	return new MondrianForest<double, functions, TREE_COUNT, FEATURES_COUNT, LABEL_COUNT, SIZE>(lifetime, base_measure, discount_factor);
 }
 
