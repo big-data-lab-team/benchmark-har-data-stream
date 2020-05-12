dd318b50ee4ee71d875908d1412eaa24f4a9444f
Martin <martin.khannouz@gmail.com>
2020-05-12 14:08:21 -0400

    Improve interface with streamdm to limit malloc usage.

--- src/streamdm_naive_bayes.cpp
+++ src/streamdm_naive_bayes.cpp
@@ -8,9 +8,10 @@ template<class feature_type, int label_count, int feature_count>
 class NaiveBayesClassifier{
 	Learner* learner = nullptr;
 	InstanceInformation* ii = nullptr;
-
+	Instance* instance = nullptr;
+	vector<double> labels, values;
 	public:
-	NaiveBayesClassifier(){
+	NaiveBayesClassifier(): labels(1), values(feature_count){
 		learner = new NaiveBayes();
 		ii = new InstanceInformation();
 		for(int i = 0; i < feature_count; ++i)
@@ -20,29 +21,27 @@ class NaiveBayesClassifier{
 			vec[i] = to_string(i);
 		}
 		ii->addClass(new Attribute(vec), 0);
+		instance = new DenseInstance();
+		instance->setInstanceInformation(ii);
 	}
 	inline bool train(feature_type const* features, int const label){
-		vector<feature_type> values(features, features+feature_count);
-		vector<double> labels(1, static_cast<double>(label));
-		Instance* instance = new DenseInstance();
-		instance->setInstanceInformation(ii);
+		for(int i = 0; i < feature_count; ++i)
+			values[i] = features[i];
+		labels[0] = label;
 		instance->addLabels(labels);
 		instance->addValues(values);
 		learner->train(*instance);
-		delete instance;
 		return true;
 	}
 	inline int predict(feature_type const* features){
-		vector<feature_type> values(features, features+feature_count);
-		Instance* instance = new DenseInstance();
-		instance->setInstanceInformation(ii);
+		for(int i = 0; i < feature_count; ++i)
+			values[i] = features[i];
 		instance->addValues(values);
 		double* predictions = learner->getPrediction(*instance);
 		int hightest = 0;
 		for(int i = 0; i < label_count; ++i)
 			if(predictions[i] > predictions[hightest])
 				hightest = i;
-		delete instance;
 		return hightest;
 	}
 };
