f12b220368cc1a0e8fec8deb8a695bd088b28239
Martin <martin.khannouz@gmail.com>
2020-06-18 12:24:48 -0400

    Update setup.

--- setup.sh
+++ setup.sh
@@ -17,6 +17,27 @@ cp bin/mondrian_t100 bin/recofit_6/mondrian_t100_double
 cp bin/mondrian_t20 bin/recofit_6/mondrian_t20_double
 cp bin/mondrian_t5 bin/recofit_6/mondrian_t5_double
 cp bin/mondrian_t50 bin/recofit_6/mondrian_t50_double
+make LABEL_COUNT=41 FEATURES_COUNT=12 BANOS=1 MEMORY_SIZE=1800000 -j 8 #recofit 6
+cp bin/mondrian_t1 bin/recofit_6/mondrian_t1_triple
+cp bin/mondrian_t10 bin/recofit_6/mondrian_t10_triple
+cp bin/mondrian_t100 bin/recofit_6/mondrian_t100_triple
+cp bin/mondrian_t20 bin/recofit_6/mondrian_t20_triple
+cp bin/mondrian_t5 bin/recofit_6/mondrian_t5_triple
+cp bin/mondrian_t50 bin/recofit_6/mondrian_t50_triple
+make LABEL_COUNT=41 FEATURES_COUNT=12 BANOS=1 MEMORY_SIZE=2400000 -j 8 #recofit 6
+cp bin/mondrian_t1 bin/recofit_6/mondrian_t1_quadruple
+cp bin/mondrian_t10 bin/recofit_6/mondrian_t10_quadruple
+cp bin/mondrian_t100 bin/recofit_6/mondrian_t100_quadruple
+cp bin/mondrian_t20 bin/recofit_6/mondrian_t20_quadruple
+cp bin/mondrian_t5 bin/recofit_6/mondrian_t5_quadruple
+cp bin/mondrian_t50 bin/recofit_6/mondrian_t50_quadruple
+make LABEL_COUNT=41 FEATURES_COUNT=12 BANOS=1 MEMORY_SIZE=3000000 -j 8 #recofit 6
+cp bin/mondrian_t1 bin/recofit_6/mondrian_t1_quintuple
+cp bin/mondrian_t10 bin/recofit_6/mondrian_t10_quintuple
+cp bin/mondrian_t100 bin/recofit_6/mondrian_t100_quintuple
+cp bin/mondrian_t20 bin/recofit_6/mondrian_t20_quintuple
+cp bin/mondrian_t5 bin/recofit_6/mondrian_t5_quintuple
+cp bin/mondrian_t50 bin/recofit_6/mondrian_t50_quintuple
 
 make BANOS=1 -j 8 #Banos
 cp bin/empty_classifier bin/mcnn_* bin/mondrian_* bin/mlp_* bin/naive_bayes bin/streamdm_* bin/banos_3
@@ -30,6 +51,27 @@ cp bin/mondrian_t100 bin/banos_6/mondrian_t100_double
 cp bin/mondrian_t20 bin/banos_6/mondrian_t20_double
 cp bin/mondrian_t5 bin/banos_6/mondrian_t5_double
 cp bin/mondrian_t50 bin/banos_6/mondrian_t50_double
+make LABEL_COUNT=33 FEATURES_COUNT=12 BANOS=1 MEMORY_SIZE=1800000 -j 8 #Banos 6 axis
+cp bin/mondrian_t1 bin/banos_6/mondrian_t1_triple
+cp bin/mondrian_t10 bin/banos_6/mondrian_t10_triple
+cp bin/mondrian_t100 bin/banos_6/mondrian_t100_triple
+cp bin/mondrian_t20 bin/banos_6/mondrian_t20_triple
+cp bin/mondrian_t5 bin/banos_6/mondrian_t5_triple
+cp bin/mondrian_t50 bin/banos_6/mondrian_t50_triple
+make LABEL_COUNT=33 FEATURES_COUNT=12 BANOS=1 MEMORY_SIZE=2400000 -j 8 #Banos 6 axis
+cp bin/mondrian_t1 bin/banos_6/mondrian_t1_quadruple
+cp bin/mondrian_t10 bin/banos_6/mondrian_t10_quadruple
+cp bin/mondrian_t100 bin/banos_6/mondrian_t100_quadruple
+cp bin/mondrian_t20 bin/banos_6/mondrian_t20_quadruple
+cp bin/mondrian_t5 bin/banos_6/mondrian_t5_quadruple
+cp bin/mondrian_t50 bin/banos_6/mondrian_t50_quadruple
+make LABEL_COUNT=33 FEATURES_COUNT=12 BANOS=1 MEMORY_SIZE=3000000 -j 8 #Banos 6 axis
+cp bin/mondrian_t1 bin/banos_6/mondrian_t1_quintuple
+cp bin/mondrian_t10 bin/banos_6/mondrian_t10_quintuple
+cp bin/mondrian_t100 bin/banos_6/mondrian_t100_quintuple
+cp bin/mondrian_t20 bin/banos_6/mondrian_t20_quintuple
+cp bin/mondrian_t5 bin/banos_6/mondrian_t5_quintuple
+cp bin/mondrian_t50 bin/banos_6/mondrian_t50_quintuple
 
 
 make LABEL_COUNT=33 FEATURES_COUNT=60 BANOS=1 -j 8
