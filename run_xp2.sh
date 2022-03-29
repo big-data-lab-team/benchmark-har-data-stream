5bfcb71b068c29f960f1c77947555b37efb45b2c
Martin <A>
2022-03-29 17:15:14 +0200

    Update run scripts

--- run_xp2.sh
+++ run_xp2.sh
@@ -19,7 +19,6 @@ runny()
 }
 
 for dataset in recofit_6 banos_6 RandomRBF_stable RandomRBF_drift covtype drift_6; do
-#for dataset in RandomRBF_stable RandomRBF_drift; do
 	mkdir $result_dir/$dataset
 	for s in `seq $rep_count`; do
 		for t in 1 5 10 20 30 50; do
@@ -36,3 +35,17 @@ for dataset in recofit_6 banos_6 RandomRBF_stable RandomRBF_drift covtype drift_
 	done
 done
 
+#Run with unbound
+for dataset in recofit_6 banos_6 RandomRBF_stable RandomRBF_drift covtype drift_6; do
+	binary="bin/$dataset/mondrian_coarse_empty_unbound"
+	mkdir $result_dir/$dataset
+	for s in (seq $rep_count); do
+		for t in 1 5 10 20 30 50; do
+			for et in original; do
+				f="mondrian_undound_t$t"_"$et"
+				echo "$binary $dataset.log $s 3 $s lifetime:0.5 base_measure:0.0 discount_factor:1.0 tree_management:phoenix tree_count:$t extend_type:$et"
+				$binary $dataset.log $s 3 $s lifetime:0.5 base_measure:0.0 discount_factor:1.0 tree_management:phoenix tree_count:$t extend_type:$et >> $result_dir/$dataset/$f.csv
+			done
+		done
+	done
+done
