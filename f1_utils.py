47b1356fd0672d57f139aac22f2c8969088777fd
Martin <martin.khannouz@gmail.com>
2022-07-04 12:54:02 +0200

    Modify f1_utils to skip error rows when reading f1 csv file.

--- f1_utils.py
+++ f1_utils.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-def read_f1(filename):
-    df = pd.read_csv(filename)
+def read_f1(filename, s=None):
+    df = pd.read_csv(filename, skiprows=1)
     df.columns = ['model_id', 'run_id', 'element_count', 'seed', 'accuracy', 'f1', 'memory']
     df = df[['element_count', 'f1']]
     # df = df[['element_count', 'accuracy']]
