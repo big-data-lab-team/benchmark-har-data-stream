7f30bd387831602d9d921178a7905c728e7e77e9
Martin <martin.khannouz@gmail.com>
2022-07-04 12:54:48 +0200

    Works on setup script

--- setup_xp3.sh
+++ setup_xp3.sh
@@ -6,23 +6,27 @@ mkdir bin/banos_6 bin/recofit_6 bin/RandomRBF_drift bin/RandomRBF_stable bin/cov
 compile () {
 	memory=$1
 	memory_name=$2
-	make LABEL_COUNT=33 FEATURES_COUNT=12 BANOS=1 -j 8 xp3 #Banos 6 axis
+	make MEMORY_SIZE=$memory LABEL_COUNT=33 FEATURES_COUNT=12 BANOS=1 -j 8 xp3 #Banos 6 axis
 	for f in bin/mondrian_*; do mv $f "${f}_${memory_name}M"; done
 	cp bin/empty_classifier bin/mondrian_* bin/banos_6
 	cp bin/empty_classifier bin/mondrian_* bin/drift_6
+	rm -f bin/empty_classifier bin/mondrian_*
 
-	make LABEL_COUNT=33 FEATURES_COUNT=12 BANOS=1 -j 8 xp3 #RandomRBF_stable and RandomRBF_drift
+	make MEMORY_SIZE=$memory LABEL_COUNT=33 FEATURES_COUNT=12 BANOS=1 -j 8 xp3 #RandomRBF_stable and RandomRBF_drift
 	for f in bin/mondrian_*; do mv $f "${f}_${memory_name}M"; done
 	cp bin/empty_classifier bin/mondrian_* bin/RandomRBF_drift
 	cp bin/empty_classifier bin/mondrian_* bin/RandomRBF_stable
+	rm -f bin/empty_classifier bin/mondrian_*
 
-	make LABEL_COUNT=7 FEATURES_COUNT=54 BANOS=1 -j 8 xp3 #covtype
-	for f in bin/mondrian_*; do mv $f "${f}_${memory_name}M"; done
-	cp bin/empty_classifier bin/mondrian_* bin/covtype
+	#make MEMORY_SIZE=$memory LABEL_COUNT=7 FEATURES_COUNT=54 BANOS=1 -j 8 xp3 #covtype
+	#for f in bin/mondrian_*; do mv $f "${f}_${memory_name}M"; done
+	#cp bin/empty_classifier bin/mondrian_* bin/covtype
+	#rm -f bin/empty_classifier bin/mondrian_*
 
-	make LABEL_COUNT=41 FEATURES_COUNT=12 BANOS=1 -j 8 xp3 #recofit 6
-	for f in bin/mondrian_*; do mv $f "${f}_${memory_name}M"; done
-	cp bin/empty_classifier bin/mondrian_* bin/recofit_6
+	#make MEMORY_SIZE=$memory LABEL_COUNT=41 FEATURES_COUNT=12 BANOS=1 -j 8 xp3 #recofit 6
+	#for f in bin/mondrian_*; do mv $f "${f}_${memory_name}M"; done
+	#cp bin/empty_classifier bin/mondrian_* bin/recofit_6
+	#rm -f bin/empty_classifier bin/mondrian_*
 }
 
 compile '200000' '0.2'
