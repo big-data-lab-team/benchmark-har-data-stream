7857d3163a0919872f7d8cc9ff0a731a89af36d6
Martin <martin.khannouz@gmail.com>
2022-08-17 15:54:00 -0400

    Update setup_xp1_xp2.sh to compile with different memory limit.

--- setup_xp1_xp2.sh
+++ setup_xp1_xp2.sh
@@ -1,48 +1,72 @@
 #!/bin/bash
 rm -rf bin
 mkdir bin
-mkdir bin/banos_3 bin/banos_6 bin/recofit_3 bin/recofit_6 bin/RandomRBF_drift bin/RandomRBF_stable bin/covtype bin/drift_3 bin/drift_6
+mkdir bin/banos_6 bin/recofit_6 bin/RandomRBF_drift bin/RandomRBF_stable bin/covtype bin/drift_6
 
-make LABEL_COUNT=33 FEATURES_COUNT=12 BANOS=1 -j 8 xp1 #Banos 6 axis
-cp bin/empty_classifier bin/mondrian_* bin/banos_6
-cp bin/empty_classifier bin/mondrian_* bin/drift_6
+compile () {
+	memory=$1
+	memory_name=$2
+	make MEMORY_SIZE=$memory LABEL_COUNT=33 FEATURES_COUNT=12 BANOS=1 -j 8 xp1 #Banos 6 axis
+	for f in bin/mondrian_*; do mv $f "${f}_${memory_name}"; done
+	mv bin/empty_classifier "bin/empty_classifier_${memory_name}"
+	cp bin/empty_classifier* bin/mondrian_* bin/banos_6
+	cp bin/empty_classifier* bin/mondrian_* bin/drift_6
+	rm -f bin/empty_classifier* bin/mondrian_*
 
-make LABEL_COUNT=33 FEATURES_COUNT=12 BANOS=1 -j 8 xp1 #RandomRBF_stable and RandomRBF_drift
-cp bin/empty_classifier bin/mondrian_* bin/RandomRBF_drift
-cp bin/empty_classifier bin/mondrian_* bin/RandomRBF_stable
+	make MEMORY_SIZE=$memory LABEL_COUNT=33 FEATURES_COUNT=12 BANOS=1 -j 8 xp1 #RandomRBF_stable and RandomRBF_drift
+	for f in bin/mondrian_*; do mv $f "${f}_${memory_name}"; done
+	mv bin/empty_classifier "bin/empty_classifier_${memory_name}"
+	cp bin/empty_classifier* bin/mondrian_* bin/RandomRBF_drift
+	cp bin/empty_classifier* bin/mondrian_* bin/RandomRBF_stable
+	rm -f bin/empty_classifier* bin/mondrian_*
 
-make LABEL_COUNT=7 FEATURES_COUNT=54 BANOS=1 -j 8 xp1 #covtype
-cp bin/empty_classifier bin/mondrian_* bin/covtype
+	make MEMORY_SIZE=$memory LABEL_COUNT=7 FEATURES_COUNT=54 BANOS=1 -j 8 xp1 #covtype
+	for f in bin/mondrian_*; do mv $f "${f}_${memory_name}"; done
+	mv bin/empty_classifier "bin/empty_classifier_${memory_name}"
+	cp bin/empty_classifier* bin/mondrian_* bin/covtype
+	rm -f bin/empty_classifier* bin/mondrian_*
 
-make BANOS=1 -j 8 xp1 #Banos
-cp bin/empty_classifier bin/mondrian_* bin/banos_3
-cp bin/empty_classifier bin/mondrian_* bin/drift_3
+	make MEMORY_SIZE=$memory LABEL_COUNT=41 FEATURES_COUNT=12 BANOS=1 -j 8 xp1 #recofit 6
+	for f in bin/mondrian_*; do mv $f "${f}_${memory_name}"; done
+	mv bin/empty_classifier "bin/empty_classifier_${memory_name}"
+	cp bin/empty_classifier* bin/mondrian_* bin/recofit_6
+	rm -f bin/empty_classifier* bin/mondrian_*
+}
+compile_opti_unbound () {
+	memory=$1
+	memory_name=$2
+	make MEMORY_SIZE=$memory UNBOUND_OPTIMIZE=1 LABEL_COUNT=33 FEATURES_COUNT=12 BANOS=1 -j 8 xp1 #Banos 6 axis
+	for f in bin/mondrian_*; do mv $f "${f}_${memory_name}"; done
+	mv bin/empty_classifier "bin/empty_classifier_${memory_name}"
+	cp bin/empty_classifier* bin/mondrian_* bin/banos_6
+	cp bin/empty_classifier* bin/mondrian_* bin/drift_6
+	rm -f bin/empty_classifier* bin/mondrian_*
 
-make LABEL_COUNT=41 FEATURES_COUNT=6 BANOS=1 -j 8 xp1 #recofit
-cp bin/empty_classifier bin/mondrian_* bin/recofit_3
+	make MEMORY_SIZE=$memory UNBOUND_OPTIMIZE=1 LABEL_COUNT=33 FEATURES_COUNT=12 BANOS=1 -j 8 xp1 #RandomRBF_stable and RandomRBF_drift
+	for f in bin/mondrian_*; do mv $f "${f}_${memory_name}"; done
+	mv bin/empty_classifier "bin/empty_classifier_${memory_name}"
+	cp bin/empty_classifier* bin/mondrian_* bin/RandomRBF_drift
+	cp bin/empty_classifier* bin/mondrian_* bin/RandomRBF_stable
+	rm -f bin/empty_classifier* bin/mondrian_*
 
-make LABEL_COUNT=41 FEATURES_COUNT=12 BANOS=1 -j 8 xp1 #recofit 6
-cp bin/empty_classifier bin/mondrian_* bin/recofit_6
+	make MEMORY_SIZE=$memory UNBOUND_OPTIMIZE=1 LABEL_COUNT=7 FEATURES_COUNT=54 BANOS=1 -j 8 xp1 #covtype
+	for f in bin/mondrian_*; do mv $f "${f}_${memory_name}"; done
+	mv bin/empty_classifier "bin/empty_classifier_${memory_name}"
+	cp bin/empty_classifier* bin/mondrian_* bin/covtype
+	rm -f bin/empty_classifier* bin/mondrian_*
 
-UNBOUND_MEMORY=2000000000
+	make MEMORY_SIZE=$memory UNBOUND_OPTIMIZE=1 LABEL_COUNT=41 FEATURES_COUNT=12 BANOS=1 -j 8 xp1 #recofit 6
+	for f in bin/mondrian_*; do mv $f "${f}_${memory_name}"; done
+	mv bin/empty_classifier "bin/empty_classifier_${memory_name}"
+	cp bin/empty_classifier* bin/mondrian_* bin/recofit_6
+	rm -f bin/empty_classifier* bin/mondrian_*
+}
 
-make MEMORY_SIZE=$UNBOUND_MEMORY LABEL_COUNT=33 FEATURES_COUNT=12 UNBOUND_OPTIMIZE=1 BANOS=1 -j 8 mondrian_coarse_empty #Banos 6 axis
-cp bin/mondrian_coarse_empty bin/banos_6/mondrian_coarse_empty_unbound
-cp bin/mondrian_coarse_empty bin/drift_6/mondrian_coarse_empty_unbound
-
-make MEMORY_SIZE=$UNBOUND_MEMORY LABEL_COUNT=33 FEATURES_COUNT=12 UNBOUND_OPTIMIZE=1 BANOS=1 -j 8 mondrian_coarse_empty #RandomRBF_stable and RandomRBF_drift
-cp bin/mondrian_coarse_empty bin/RandomRBF_drift/mondrian_coarse_empty_unbound
-cp bin/mondrian_coarse_empty bin/RandomRBF_stable/mondrian_coarse_empty_unbound
-
-make MEMORY_SIZE=$UNBOUND_MEMORY LABEL_COUNT=7 FEATURES_COUNT=54 UNBOUND_OPTIMIZE=1 BANOS=1 -j 8 mondrian_coarse_empty #covtype
-cp bin/mondrian_coarse_empty bin/covtype/mondrian_coarse_empty_unbound
-
-make MEMORY_SIZE=$UNBOUND_MEMORY UNBOUND_OPTIMIZE=1 BANOS=1 -j 8 mondrian_coarse_empty #Banos
-cp bin/mondrian_coarse_empty bin/banos_3/mondrian_coarse_empty_unbound
-cp bin/mondrian_coarse_empty bin/drift_3/mondrian_coarse_empty_unbound
-
-make MEMORY_SIZE=$UNBOUND_MEMORY LABEL_COUNT=41 FEATURES_COUNT=6 UNBOUND_OPTIMIZE=1 BANOS=1 -j 8 mondrian_coarse_empty #recofit
-cp bin/mondrian_coarse_empty bin/recofit_3/mondrian_coarse_empty_unbound
-
-make MEMORY_SIZE=$UNBOUND_MEMORY LABEL_COUNT=41 FEATURES_COUNT=12 UNBOUND_OPTIMIZE=1 BANOS=1 -j 8 mondrian_coarse_empty #recofit 6
-cp bin/mondrian_coarse_empty bin/recofit_6/mondrian_coarse_empty_unbound
+compile '600000' '0.6M'
+compile '1000000' '1M'
+compile '2000000' '2M'
+compile '10000000' '10M'
+compile '50000000' '50M'
+compile '100000000' '100M'
+compile '200000000' '200M'
+compile_opti_unbound '2000000000' '2G'
