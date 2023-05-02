e45796d9002d488a1b064954cda2b12a9e050f28
Martin <A>
2023-05-02 18:28:59 -0400

    Add parallel run for xp1 and xp2

--- run_xp1_parallel.sh
+++ run_xp1_parallel.sh
@@ -16,7 +16,11 @@ runny()
 	local iteration_dir=$6
 	local binary="bin/$dataset/mondrian_coarse_empty_"$memory
 	local f="mondrian_t$tree_count"_"$extend_type"_"$memory"
-	echo "$binary $dataset.log $seed 3 $seed lifetime:0.5 base_measure:0.0 discount_factor:1.0 tree_management:phoenix tree_count:$tree_count extend_type:$extend_type >> $result_dir/$dataset/$iteration_dir/$f.csv" >> commands_$ID.txt
+	if [ "$iteration_dir" -le "11" ]; then
+		echo "$binary $dataset.log $seed 3 $seed lifetime:0.5 base_measure:0.0 discount_factor:1.0 tree_management:phoenix tree_count:$tree_count extend_type:$extend_type >> $result_dir/$dataset/$iteration_dir/$f.csv" >> commands_$ID.txt
+	else
+		echo "$binary $dataset.log $seed 3 $seed lifetime:0.5 base_measure:0.0 discount_factor:1.0 tree_management:phoenix tree_count:$tree_count extend_type:$extend_type | tail -n 1 >> $result_dir/$dataset/$iteration_dir/$f.csv" >> commands_$ID.txt
+	fi
 }
 runny_generated()
 {
@@ -28,13 +32,17 @@ runny_generated()
 	local iteration_dir=$6
 	local binary="bin/$dataset/mondrian_coarse_empty_"$memory
 	local f="mondrian_t$tree_count"_"$extend_type"_"$memory"
-	echo "$binary datasets/$dataset.log $seed 3 $seed lifetime:0.5 base_measure:0.0 discount_factor:1.0 tree_management:phoenix tree_count:$tree_count extend_type:$extend_type >> $result_dir/$dataset/$iteration_dir/$f.csv" >> commands_$ID.txt
-
+	if [ "$iteration_dir" -le "11" ]; then
+		echo "$binary datasets/$dataset.log $seed 3 $seed lifetime:0.5 base_measure:0.0 discount_factor:1.0 tree_management:phoenix tree_count:$tree_count extend_type:$extend_type  >> $result_dir/$dataset/$iteration_dir/$f.csv" >> commands_$ID.txt
+	else
+		echo "$binary datasets/$dataset.log $seed 3 $seed lifetime:0.5 base_measure:0.0 discount_factor:1.0 tree_management:phoenix tree_count:$tree_count extend_type:$extend_type | tail -n 1  >> $result_dir/$dataset/$iteration_dir/$f.csv" >> commands_$ID.txt
+	fi
 }
 #Run all out-of-memory strategy for validation generated datasets
 for s in $rep_test; do
 	for memory in '0.6M'; do
-		for d in datasets/*.log; do
+		#for d in datasets/*.log; do
+		for d in datasets/hyperplane_0_4_001_01.*; do
 			dataset=`basename $d .log`
 			[ -d $result_dir/$dataset ] || mkdir $result_dir/$dataset
 			[ -d $result_dir/$dataset/$s ] || mkdir $result_dir/$dataset/$s
@@ -49,8 +57,24 @@ done
 
 #Run all out-of-memory strategy
 for s in $rep_test; do
-	for memory in '0.6M' '1M' '10M' '50M' '100M' '200M'; do
-		for dataset in recofit_6 banos_6 RandomRBF_stable RandomRBF_drift covtype drift_6; do
+	for memory in '0.6M'; do
+		for dataset in recofit_6 banos_6 RandomRBF_stable RandomRBF_drift covtype drift_6 pamap_chest harth har70; do
+			[ -d $result_dir/$dataset ] || mkdir $result_dir/$dataset
+			[ -d $result_dir/$dataset/$s ] || mkdir $result_dir/$dataset/$s
+			for t in 1 5 10 20 30 50; do
+				for et in none original count_only partial ghost; do
+					runny $s $t $et $dataset $memory $s
+				done
+			done
+		done
+	done
+done
+
+#Run all out-of-memory strategy with more memory
+#Only run 10 repetitions 
+for s in ${rep_test[@]:0:10}; do
+	for memory in '1M' '10M' '50M' '100M' '200M'; do
+		for dataset in recofit_6 banos_6 RandomRBF_stable RandomRBF_drift covtype drift_6 pamap_chest harth har70; do
 			[ -d $result_dir/$dataset ] || mkdir $result_dir/$dataset
 			[ -d $result_dir/$dataset/$s ] || mkdir $result_dir/$dataset/$s
 			for t in 1 5 10 20 30 50; do
@@ -63,19 +87,24 @@ for s in $rep_test; do
 done
 
 #Run with unbound
-for dataset in recofit_6 banos_6 RandomRBF_stable RandomRBF_drift covtype drift_6; do
+for dataset in recofit_6 banos_6 RandomRBF_stable RandomRBF_drift covtype drift_6 pamap_chest harth har70; do
 	binary="bin/$dataset/mondrian_coarse_empty_2G"
 	[ -d $result_dir/$dataset ] || mkdir $result_dir/$dataset
 	[ -d $result_dir/$dataset/$s ] || mkdir $result_dir/$dataset/$s
 	for s in $rep_test; do
 		for t in 1 5 10 20 30 50; do
 			for et in original; do
-				f="mondrian_undound_t$t"_"$et"
-				echo "$binary $dataset.log $s 3 $s lifetime:0.5 base_measure:0.0 discount_factor:1.0 tree_management:phoenix tree_count:$t extend_type:$et >> $result_dir/$dataset/$s/$f.csv" >> commands_$ID.txt
+				f="mondrian_unbound_t$t"_"$et"
+				if [ "$s" -le "11" ]; then
+					echo "$binary $dataset.log $s 3 $s lifetime:0.5 base_measure:0.0 discount_factor:1.0 tree_management:phoenix tree_count:$t extend_type:$et >> $result_dir/$dataset/$s/$f.csv" >> commands_$ID.txt
+				else
+					echo "$binary $dataset.log $s 3 $s lifetime:0.5 base_measure:0.0 discount_factor:1.0 tree_management:phoenix tree_count:$t extend_type:$et | tail -n 1 >> $result_dir/$dataset/$s/$f.csv" >> commands_$ID.txt
+				fi
 			done
 		done
 	done
 done
 
 echo "Done Listing; Start Running"
-#parallel --memfree 4G --retries 10 --progress < commands_$ID.txt
+parallel --memfree 4G --retries 10 --progress < commands_$ID.txt
+rm -f commands_$ID.txt
--- run_xp2_parallel.sh
+++ run_xp2_parallel.sh
@@ -4,7 +4,7 @@ result_dir="results_xp2"
 rep_test="$@"
 [ -d $result_dir ] || mkdir $result_dir
 
-declare -A dataset_last_data_point=( [banos_6]=11050 [RandomRBF_stable]=17050 [RandomRBF_drift]=17050 [drift_6]=11050 [recofit_6]=80000 [covtype]=570000)
+declare -A dataset_last_data_point=( [banos_6]=11050 [RandomRBF_stable]=17050 [RandomRBF_drift]=17050 [drift_6]=11050 [recofit_6]=80000 [covtype]=570000 [pamap_chest]=15000 [harth]=120000 [har70]=42000)
 
 #Add other datasets if necessary
 for dataset in datasets/*.log; do
@@ -26,7 +26,11 @@ runny()
 	local iteration_dir=$8
 	local binary="bin/$dataset/mondrian_coarse_empty_"$memory
 	local f="mondrian_t"$t"_"$et"_"$sh"_"$tt"_"$memory
-	echo "$binary $dataset.log $s 3 $s lifetime:0.5 base_measure:0.0 discount_factor:1.0 tree_management:phoenix tree_count:$t extend_type:$et split_helper:$sh trim_type:$tt maximum_trim_size:0.03 > $result_dir/$dataset/$iteration_dir/$f.csv" >> commands_$ID.txt
+	if [ "$iteration_dir" -le 11 ]; then
+		echo "$binary $dataset.log $s 3 $s lifetime:0.5 base_measure:0.0 discount_factor:1.0 tree_management:phoenix tree_count:$t extend_type:$et split_helper:$sh trim_type:$tt maximum_trim_size:0.03 > $result_dir/$dataset/$iteration_dir/$f.csv" >> commands_$ID.txt
+	else
+		echo "$binary $dataset.log $s 3 $s lifetime:0.5 base_measure:0.0 discount_factor:1.0 tree_management:phoenix tree_count:$t extend_type:$et split_helper:$sh trim_type:$tt maximum_trim_size:0.03 | tail -n 1 > $result_dir/$dataset/$iteration_dir/$f.csv" >> commands_$ID.txt
+	fi
 }
 runny_generated()
 {
@@ -40,7 +44,11 @@ runny_generated()
 	local iteration_dir=$8
 	local binary="bin/$dataset/mondrian_coarse_empty_"$memory
 	local f="mondrian_t"$t"_"$et"_"$sh"_"$tt"_"$memory
-	echo "$binary datasets/$dataset.log $s 3 $s lifetime:0.5 base_measure:0.0 discount_factor:1.0 tree_management:phoenix tree_count:$t extend_type:$et split_helper:$sh trim_type:$tt maximum_trim_size:0.03 > $result_dir/$dataset/$iteration_dir/$f.csv" >> commands_$ID.txt
+	if [ "$iteration_dir" -le 11 ]; then
+		echo "$binary datasets/$dataset.log $s 3 $s lifetime:0.5 base_measure:0.0 discount_factor:1.0 tree_management:phoenix tree_count:$t extend_type:$et split_helper:$sh trim_type:$tt maximum_trim_size:0.03 > $result_dir/$dataset/$iteration_dir/$f.csv" >> commands_$ID.txt
+	else
+		echo "$binary datasets/$dataset.log $s 3 $s lifetime:0.5 base_measure:0.0 discount_factor:1.0 tree_management:phoenix tree_count:$t extend_type:$et split_helper:$sh trim_type:$tt maximum_trim_size:0.03 | tail -n 1 > $result_dir/$dataset/$iteration_dir/$f.csv" >> commands_$ID.txt
+	fi
 }
 runny_2G()
 {
@@ -50,8 +58,12 @@ runny_2G()
 	local dataset=$4
 	local iteration_dir=$1 #the iteration_dir is the seed name
 	local binary="bin/$dataset/mondrian_coarse_empty_2G"
-	local f="mondrian_undound_t$t"_"$et"
-	echo "$binary $dataset.log $s 3 $s lifetime:0.5 base_measure:0.0 discount_factor:1.0 tree_management:phoenix tree_count:$t extend_type:$et > $result_dir/$dataset/$iteration_dir/$f.csv" >> commands_$ID.txt
+	local f="mondrian_unbound_t$t"_"$et"
+	if [ "$iteration_dir" -le 11 ]; then
+		echo "$binary $dataset.log $s 3 $s lifetime:0.5 base_measure:0.0 discount_factor:1.0 tree_management:phoenix tree_count:$t extend_type:$et > $result_dir/$dataset/$iteration_dir/$f.csv" >> commands_$ID.txt
+	else
+		echo "$binary $dataset.log $s 3 $s lifetime:0.5 base_measure:0.0 discount_factor:1.0 tree_management:phoenix tree_count:$t extend_type:$et | tail -n 1 > $result_dir/$dataset/$iteration_dir/$f.csv" >> commands_$ID.txt
+	fi
 }
 
 echo "Listing Commands"
@@ -76,9 +88,32 @@ for s in $rep_test; do
 	done
 done
 
+#Run all trimming strategies
 for s in $rep_test; do
-	for memory in '0.6M' '1M' '10M' '100M' '200M'; do
-		for dataset in recofit_6 banos_6 RandomRBF_stable RandomRBF_drift covtype drift_6; do
+	for memory in '0.6M'; do
+		for dataset in recofit_6 banos_6 RandomRBF_stable RandomRBF_drift covtype drift_6 pamap_chest harth har70; do
+			#Create the directories if they don't exist
+			[ -d $result_dir/$dataset ] || mkdir $result_dir/$dataset
+			[ -d $result_dir/$dataset/$s ] || mkdir $result_dir/$dataset/$s
+			for t in 1 5 10 20 30 50; do
+				for extend_type in original,none barycenter,weighted barycenter,avg; do
+					for tt in none random fading_score count; do
+						IFS="," read -r -a array <<< $extend_type
+						et=${array[0]}
+						sh=${array[1]}
+						runny $s $t $et $sh $tt $dataset $memory $s 
+					done
+				done
+			done
+		done
+	done
+done
+
+#Run all trimming strategies
+#Only 10 rep with more memory
+for s in ${rep_test[@]:0:10}; do
+	for memory in '1M' '10M' '100M' '200M'; do
+		for dataset in recofit_6 banos_6 RandomRBF_stable RandomRBF_drift covtype drift_6 pamap_chest harth har70; do
 			#Create the directories if they don't exist
 			[ -d $result_dir/$dataset ] || mkdir $result_dir/$dataset
 			[ -d $result_dir/$dataset/$s ] || mkdir $result_dir/$dataset/$s
@@ -97,7 +132,7 @@ for s in $rep_test; do
 done
 
 #Run with unbound
-for dataset in recofit_6 banos_6 RandomRBF_stable RandomRBF_drift covtype drift_6; do
+for dataset in recofit_6 banos_6 RandomRBF_stable RandomRBF_drift covtype drift_6 pamap_chest harth har70; do
 	[ -d $result_dir/$dataset ] || mkdir $result_dir/$dataset
 	[ -d $result_dir/$dataset/$s ] || mkdir $result_dir/$dataset/$s
 	for s in $rep_test; do
@@ -109,21 +144,7 @@ for dataset in recofit_6 banos_6 RandomRBF_stable RandomRBF_drift covtype drift_
 	done
 done
 
+
+#echo "Done Listing; Start Running"
+#parallel --memfree 4G --retries 10 --progress < commands_$ID.txt
 #rm -f commands_$ID.txt
-
-#Run with unbound
-#for d in datasets/*.log; do
-#	datasets=`basename $d .log`
-#	[ -d $result_dir/$dataset ] || mkdir $result_dir/$dataset
-#	[ -d $result_dir/$dataset/$s ] || mkdir $result_dir/$dataset/$s
-#	for s in $rep_test; do
-#		for t in 1 5 10 20 30 50; do
-#			for et in original; do
-#				runny_2G $s $t $et $dataset 
-#			done
-#		done
-#	done
-#done
-
-echo "Done Listing; Start Running"
-parallel --memfree 4G --retries 10 --progress < commands_$ID.txt
